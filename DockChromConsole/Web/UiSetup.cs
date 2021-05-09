77589afe7726a8e94b318cfd2b30be845036a33f
Surya Rekha <surya.rekha@rebtel.com>
2021-05-09 11:55:17 +0200

    test

--- DockChromConsole/Web/UiSetup.cs
+++ DockChromConsole/Web/UiSetup.cs
@@ -56,6 +56,7 @@ namespace uitest.browser
         {
             var driver = InitDriver(browser);
             driver.Url = url;
+            Console.WriteLine("Opening webpage: "+url);
             return driver;
         }
 
